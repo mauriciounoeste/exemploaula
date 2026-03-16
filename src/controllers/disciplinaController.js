6c96707f6228a02a05c8f2cd3a876d6634131bdf
mauriciounoeste <149174057+mauriciounoeste@users.noreply.github.com>
2026-03-16 20:23:15 -0300

    Update disciplinaController.js

--- src/controllers/disciplinaController.js
+++ src/controllers/disciplinaController.js
@@ -1,1 +1,13 @@
 
+const pool = require('../config/db');
+
+async function listar(req, res) {
+  try {
+    const [rows] = await pool.query('SELECT * FROM disciplinas');
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao buscar disciplinas', detalhe: error.message });
+  }
+}
+
+module.exports = { listar };
